76224863d4f91e20e636644d9ea608dbf56cb23b
teligramboturl <teligramboturl@gmail.com>
2025-11-17 01:37:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -845,6 +845,5 @@ async def upload(bot: Client, m: Message):
     await m.reply_text(f"<pre><code>📥𝗘𝘅𝘁𝗿𝗮𝗰𝘁𝗲𝗱 𝗕𝘆 ➤『{CR}』</code></pre>")
     await m.reply_text(f"<pre><code>『😏𝗥𝗲𝗮𝗰𝘁𝗶𝗼𝗻 𝗞𝗼𝗻 𝗗𝗲𝗴𝗮😏』</code></pre>")                 
 
-bot.run()
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot.run()
